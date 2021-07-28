7375fdfe214fc4d0cf6f7e0019de8bd77a84dbea
mamadhss <mamadhss@yahoo.com>
2021-07-28 12:10:07 +0430

    first

---
+++ Components/Todo.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { View, Text, Button,StyleSheet } from 'react-native';
+const Todo = (props) => {
+    return(
+        <View style={[ {margin:8,padding:0},styles.item ]}>
+            <Text>{props.item}</Text>
+            <Button
+                title={'Delete'}
+                color={'red'}
+                onPress = {() => props.delete(props.item)}
+            
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: 'whitesmoke'
+
+    }
+})
+
+
+export default Todo
+
+
